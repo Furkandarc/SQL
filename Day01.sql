da4b3f305df6c77687d61f92eae93eef41bb78a2
Furkan Darıcı <fdrc2002@gmail.com>
2022-12-02 00:29:34 +0300

    01/12/22

---
+++ Day01.sql
@@ -0,0 +1,36 @@
+--Database olusturma
+Create database darc;
+
+--DDL - DATA DEFINITION LANG.
+--CREATE - TABLO OLUSTURMA
+CREATE TABLE ogrenciler
+(
+ogrenci_no char(7),
+isim varchar(20),
+soyisim varchar(25),
+not_ortalaması real, --Ondalıklı sayılar için kullanılır(Double gibi)
+kayit_tarihi date
+);
+
+--VAROLAN TABLODAN YENİ BİR TABLO OLUŞTURMA
+CREATE TABLE ogrenci_notlari
+as --Benzer tablodaki başlıklarla ve data tipleriyle yeni bir tablo olusturmak icin kullanılır.
+select isim,soyisim,not_ortalaması,kayit_tarihi from ogrenciler;
+
+-- DML - DATA MANUPULATION LANG.
+-- INSERT (Database'e veri ekleme)
+
+INSERT INTO ogrenciler VALUES (1234567,'Furkan','Darıcı',85.5,now());
+INSERT INTO ogrenciler VALUES (1234567,'Furkan','Darıcı',85.5,'2022-12-1');
+
+-- BIR TABLOYA PARCALI VERI EKLEMEK ISTERSEK
+
+INSERT INTO ogrenciler (isim,soyisim) VALUES ('Furkan','Darıcı')
+
+-- DQL - DATA QUERY LANG.
+-- SELECT
+
+select * FROM ogrenciler; --Burdaki * sembolü herşey anlamındadır
+
+
+
