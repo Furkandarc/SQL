0d5bdb29a77c989fc451285fa3e7796efb14242e
Furkan Darıcı <fdrc2002@gmail.com>
2022-12-07 00:20:32 +0300

    06/12/2022

--- Day05.sql
+++ Day05.sql
@@ -33,7 +33,10 @@ SELECT isim FROM personel GROUP BY isim
 
 
 --HAVING:
---SADECE GROUP BY ILE KULLANILIR, WHERE YERINE KULLANILIR, SONA YAZILIR.
+/*
+	Having komutu yalnizca GROUP BY komutu ile kullanilir
+	Eger gruplamadan sonra bir sart varsa HAVING komutu kullanilir
+*/
 
 --1) Her sirketin MIN maaslarini eger 4000’den buyukse goster
 SELECT * FROM personel
@@ -41,3 +44,112 @@ SELECT * FROM personel
 SELECT sirket,min(maas) as en_dusuk_maas FROM personel
 GROUP BY sirket
 HAVING min(maas)>4000
+
+-- Ayni isimdeki kisilerin aldigi toplam gelir 10000 liradan fazla ise ismi ve toplam maasi gosteren sorgu yaziniz
+SELECT isim,sum(maas) as toplam_maas FROM personel
+GROUP BY isim
+HAVING sum(maas)>10000
+
+-- Eger bir sehirde calisan personel sayisi 1’den coksa sehir ismini ve personel sayisini veren sorgu yaziniz
+SELECT sehir,count(isim) as toplam_personel_sayisi FROM personel
+GROUP BY sehir
+HAVING count(isim)>1
+
+-- 4) Eger bir sehirde alinan MAX maas 5000’den dusukse
+-- sehir ismini ve MAX maasi veren sorgu yaziniz
+SELECT sehir,MAX(maas) as en_yuksek_maas FROM personel
+GROUP BY sehir
+HAVING MAX(maas)<5000
+
+SELECT * FROM personel
+
+
+--UNION OPERATOR
+
+-- 1) Maasi 4000’den cok olan isci isimlerini ve 5000 liradan fazla maas alinan
+-- sehirleri gosteren sorguyu yaziniz
+SELECT isim,maas FROM personel WHERE maas>4000
+UNION
+SELECT sehir,maas FROM personel WHERE maas>5000
+
+--Mehmet Ozturk ismindeki kisilerin aldigi maaslari ve Istanbul’daki personelin maaslarini
+--bir tabloda gosteren sorgu yaziniz
+SELECT isim as isim_ve_sehir,maas FROM personel WHERE isim = 'Mehmet Ozturk'
+UNION
+SELECT sehir,maas FROM personel WHERE sehir = 'Istanbul'
+
+DROP TABLE if exists personel
+
+CREATE TABLE personel
+(
+id int,
+isim varchar(50),  
+sehir varchar(50), 
+maas int,  
+sirket varchar(20),
+CONSTRAINT personel_pk PRIMARY KEY (id)
+);
+INSERT INTO personel VALUES(123456789, 'Ali Yilmaz', 'Istanbul', 5500, 'Honda');  
+INSERT INTO personel VALUES(234567890, 'Veli Sahin', 'Istanbul', 4500, 'Toyota');  
+INSERT INTO personel VALUES(345678901, 'Mehmet Ozturk', 'Ankara', 3500, 'Honda');  
+INSERT INTO personel VALUES(456789012, 'Mehmet Ozturk', 'Izmir', 6000, 'Ford');  
+INSERT INTO personel VALUES(567890123, 'Mehmet Ozturk', 'Ankara', 7000, 'Tofas');  
+INSERT INTO personel VALUES(456715012, 'Veli Sahin', 'Ankara', 4500, 'Ford');  
+INSERT INTO personel VALUES(123456710, 'Hatice Sahin', 'Bursa', 4500, 'Honda');
+
+
+CREATE TABLE personel_bilgi
+(
+id int,
+tel char(10) UNIQUE,  
+cocuk_sayisi int,
+CONSTRAINT personel_bilgi_fk FOREIGN KEY (id) REFERENCES personel(id)
+);
+INSERT INTO personel_bilgi VALUES(123456789, '5302345678', 5);  
+INSERT INTO personel_bilgi VALUES(234567890, '5422345678', 4);
+INSERT INTO personel_bilgi VALUES(345678901, '5354561245', 3);
+INSERT INTO personel_bilgi VALUES(456789012, '5411452659', 3);
+INSERT INTO personel_bilgi VALUES(567890123, '5551253698', 2);
+INSERT INTO personel_bilgi VALUES(456789012, '5524578574', 2);
+INSERT INTO personel_bilgi VALUES(123456710, '5537488585', 1);
+
+select * from personel
+select * from personel_bilgi
+
+-- id’si 123456789 olan personelin    Personel tablosundan sehir ve maasini, 
+-- personel_bilgi  tablosundan da tel ve cocuk sayisini yazdirin
+SELECT sehir as sehir_ve_tel,maas as maas_ve_cocuk_sayisi FROM personel WHERE id = 123456789
+UNION
+SELECT tel,cocuk_sayisi FROM personel_bilgi WHERE id = 123456789
+
+-- UNION ALL
+/*
+Union tekrarli verileri teke düşürür ve bize o şekilde sonuç verir
+Union All ise tekrarli verilerle birlikte tün sorguları getirir
+*/
+
+-- Personel tablosunda maasi 5000'den az olan tum isimleri ve maaslari bulunuz
+
+SELECT isim,maas FROM personel WHERE maas<5000
+UNION ALL
+SELECT isim,maas FROM personel WHERE maas<5000
+
+
+--INTERSECT (Kesişim)
+
+
+--Farkli iki tabloda'ki verileri INTERSECT komutu ile getirebiliriz
+
+
+-- Personel tablosundan Istanbul veya Ankara’da calisanlarin id’lerini yazdir
+--Personel_bilgi tablosundan 2 veya 3 cocugu olanlarin id lerini yazdirin
+--Iki sorguyu INTERSECT ile birlestirin
+
+SELECT id FROM personel WHERE sehir IN ('Istanbul','Ankara')
+INTERSECT
+SELECT id FROM personel_bilgi WHERE cocuk_sayisi IN (2,3)
+
+-- Honda,Ford ve Tofas’ta calisan ortak isimde personel varsa listeleyin
+SELECT isim FROM personel WHERE sirket='Honda'
+SELECT isim FROM personel WHERE sirket='Ford'
+SELECT isim FROM personel WHERE sirket='Tofas'
